f92e8163b38283cb4e433f12049b271bf74306c3
robin <robin.kallen@student.pxl.be>
2021-03-29 16:20:02 +0200

    implementation DaoImpl Tests

--- src/test/java/be/pxl/auctions/dao/impl/UserDaoImplTest.java
+++ src/test/java/be/pxl/auctions/dao/impl/UserDaoImplTest.java
@@ -10,9 +10,11 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
@@ -45,21 +47,55 @@ public class UserDaoImplTest {
 	@Test
 	public void userCanBeSavedAndRetrievedByEmail() {
 		// TODO implement this test
+		User user = new User();
+		user.setFirstName("Robin");
+		user.setLastName("Kall");
+		user.setEmail("robin.kall@example.com");
+		user.setDateOfBirth(LocalDate.of(2000, 10, 6));
+		String email = userDao.saveUser(user).getEmail();
+		entityManager.flush();
+		entityManager.clear();
+		Optional<User> retrievedUser = userDao.findUserByEmail(email);
+		assertTrue(retrievedUser.isPresent());
+		assertEquals(user.getFirstName(), retrievedUser.get().getFirstName());
+		assertEquals(user.getLastName(), retrievedUser.get().getLastName());
+		assertEquals(user.getEmail(), retrievedUser.get().getEmail());
+		assertEquals(user.getDateOfBirth(), retrievedUser.get().getDateOfBirth());	
+
 	}
 
 	@Test
 	public void returnsNullWhenNoUserFoundWithGivenEmail() {
-		// TODO implement this test
+		User user = new User();
+		user.setFirstName("Robin");
+		user.setLastName("Kall");
+		user.setEmail("robin.kall@example.com");
+		user.setDateOfBirth(LocalDate.of(2000, 10, 6));
+		entityManager.flush();
+		entityManager.clear();
+		Optional<User> retrievedUser = userDao.findUserByEmail("not.existing@email.example");
+		assertFalse(retrievedUser.isPresent());
 	}
 
 	@Test
 	public void allUsersCanBeRetrieved() {
-		// TODO implement this test
 		// create and save one user
-
+		User user = new User();
+		user.setFirstName("Robin");
+		user.setLastName("Kall");
+		user.setEmail("robin.kall@example.com");
+		user.setDateOfBirth(LocalDate.of(2000, 10, 6));
+		String email = userDao.saveUser(user).getEmail();
+		entityManager.flush();
+		entityManager.clear();
 		// retrieve all users
+		ArrayList<User> users = new ArrayList<User>();
+		users.addAll(userDao.findAllUsers());
 		// make sure there is at least 1 user in the list
+		assertTrue(users.size() >= 1);
 		// make sure the newly created user is in the list (e.g. test if a user with this email address is in the list)
+		long usersWithCreatedEmail = users.stream().filter(u -> u.getEmail().equals(email)).count();
+		assertEquals(1, (int)usersWithCreatedEmail);
 	}
 
 
