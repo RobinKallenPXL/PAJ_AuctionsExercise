88488d682f5c3bde56dfd4e5e34b571804d5c18c
robin <robin.kallen@student.pxl.be>
2021-03-29 16:19:36 +0200

    implementation email test

--- src/test/java/be/pxl/auctions/util/EmailValidatorIsValidTest.java
+++ src/test/java/be/pxl/auctions/util/EmailValidatorIsValidTest.java
@@ -1,5 +1,8 @@
 package be.pxl.auctions.util;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class EmailValidatorIsValidTest {
@@ -7,11 +10,17 @@ public class EmailValidatorIsValidTest {
 	@Test
 	public void returnsTrueWhenValidEmail() {
 		// TODO implement test
+		assertTrue(EmailValidator.isValid("robin.kallen@student.pxl.be"));
 	}
 
 	@Test
 	public void returnsFalseWhenAtSignMissing() {
 		// TODO implement test
+		assertFalse(EmailValidator.isValid("lol not an email"));
+		/*
+		assertFalse(EmailValidator.isValid("Lol not @ctually an email.com"));
+		assertFalse(EmailValidator.isValid("not@an@email.com")); 
+		*/
 	}
 
 }
